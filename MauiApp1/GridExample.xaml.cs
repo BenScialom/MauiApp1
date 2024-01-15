6d08cd6f0fe120209782269c6af9e2c641592b97
User <User@LAB3-3>
2024-01-15 15:10:14 +0200

    jhvds

--- MauiApp1/GridExample.xaml.cs
+++ MauiApp1/GridExample.xaml.cs
@@ -3,8 +3,36 @@ namespace MauiApp1;
 
 public partial class GridExample : ContentPage
 {
+	int x;
 	public GridExample()
 	{
 		InitializeComponent();
+
 	}
+	private void Print(object sender, EventArgs e)
+	{
+		a1.Text += sender.GetType();
+
+	}
+
+    private void a1_Clicked(object sender, EventArgs e)
+    {
+        Button btn= (Button)sender;
+
+    }
+
+    private void a2_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void a3_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Minus_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
